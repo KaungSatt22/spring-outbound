9e272faa10ca688a0cfaef8de4b0b11ff898dea3
K <kaungsatt.2k1@gmail.com>
2024-07-31 09:23:23 +0630

    project setup

---
+++ src/main/java/com/outbound/spring_outbound/SpringOutboundApplication.java
@@ -0,0 +1,13 @@
+package com.outbound.spring_outbound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOutboundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOutboundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outbound/spring_outbound/SpringOutboundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outbound.spring_outbound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOutboundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
